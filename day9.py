c40b2a055c2506589af45de6d479a2caf2b83532
lkuszal <lkuszal1@gmail.com>
2020-12-09 21:25:39 +0100

    day9

--- day9.py
+++ day9.py
@@ -1,3 +1,6 @@
+import time
+
+t1 = time.perf_counter()
 src = open('input9.txt', 'r')
 inp = src.readlines()
 # creating list preamb with first 25 numbers and nextval with other ones
@@ -16,22 +19,51 @@ for x in range(len(preamb)):
     for y in range(x):
         a.append(preamb[x] + preamb[y])
     summatrix.append(a)
-
+''' I'll come back for you
 # creating vect with new sums, updating preamb and summatrix
 print(nextval)
 for x in range(len(nextval)):
-    if not any(nextval[x] in z for z in summatrix):
-        print(nextval[x])
-        break
-    n = x % 24
+    #if not any(nextval[x] in z for z in summatrix):
+     #   print(nextval[x])
+      #  break
+    n = x % 25
     vect = []
-    preamb.pop(0)
-    preamb.append(nextval[x])
+    preamb.pop(n)
     for a in preamb[:-1]:
         vect.append(a + nextval[x])
+    preamb.insert(n,nextval[x])
     # horizontal list
-    summatrix[n+1] = vect[:n+1]
+    summatrix[n] = vect[:n+1]
     # vertical list
     for y in range(n+1, len(vect)):
         summatrix[y+1][n+1] = vect[y]
-
+    print(vect)
+'''
+for z in nextval:
+    if not any(z in d for d in summatrix):
+        weakness=z
+        print(weakness)
+        break
+    summatrix=[]
+    preamb.pop(0)
+    preamb.append(z)
+    for x in range(len(preamb)):
+        a = []
+        for y in range(x):
+            a.append(preamb[x] + preamb[y])
+        summatrix.append(a)
+#part two
+for x in range(len(nextval)):
+    check=nextval[x]
+    n=1
+    res=[nextval[x]]
+    while check < weakness:
+        check+=nextval[x+n]
+        res.append(nextval[x + n])
+        n+=1
+    else:
+        if check==weakness:
+            print(min(res)+max(res))
+            break
+t2=time.perf_counter()
+print(t2-t1)
